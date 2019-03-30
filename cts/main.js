d64057e7298cb601336293c7c0e40393a9f876ca
ariffrahim <43535752+ariffrahim@users.noreply.github.com>
2019-03-30 15:47:59 +0800

    Add time function

--- cts/main.js
+++ cts/main.js
@@ -21,3 +21,32 @@ function remove_field1(id)
 {
   document.getElementById(id+"_wrapper").innerHTML="";
 }
+
+	var countDownDate = localStorage.getItem("setTime");
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+	// Get todays date and time
+	var now = new Date().getTime();
+    
+	// Find the distance between now and the count down date
+	var distance = countDownDate - now;
+    
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+	// Output the result in an element with id="demo"
+	document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+	+ minutes + "m " + seconds + "s ";
+    
+	// If the count down is over, write some text 
+	if (distance < 0) {
+		clearInterval(x);
+		//habis masa
+		document.getElementById("demo").innerHTML = "EXPIRED";
+	}
+	}, 1000);
